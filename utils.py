fa84920188149a3b8b66b99206a7e4ccbaa5e481
yokamoto5742-h <yokamoto5742@gmail.com>
2025-02-25 18:48:39 +0900

    utils.pyの更新

--- utils.py
+++ utils.py
@@ -2,21 +2,6 @@ from datetime import datetime
 
 
 def parse_date_safely(date_str, format_str='%Y-%m-%d'):
-    """
-    文字列を安全にdatetimeオブジェクトに変換する
-    
-    Parameters:
-    -----------
-    date_str : str
-        変換する日付文字列
-    format_str : str
-        日付のフォーマット
-        
-    Returns:
-    --------
-    datetime or None
-        変換に成功した場合はdatetimeオブジェクト、失敗した場合はNone
-    """
     try:
         return datetime.strptime(date_str, format_str)
     except (ValueError, TypeError):
@@ -24,19 +9,6 @@ def parse_date_safely(date_str, format_str='%Y-%m-%d'):
 
 
 def safe_float_conversion(value):
-    """
-    値を安全にfloatに変換する
-    
-    Parameters:
-    -----------
-    value : any
-        変換する値
-        
-    Returns:
-    --------
-    float or None
-        変換に成功した場合はfloat値、失敗した場合はNone
-    """
     if isinstance(value, (int, float)):
         return float(value)
     elif isinstance(value, str):
@@ -49,19 +21,6 @@ def safe_float_conversion(value):
 
 
 def extract_name_from_content(content):
-    """
-    内容から名前を抽出する（括弧内の文字列）
-    
-    Parameters:
-    -----------
-    content : str
-        抽出元の文字列
-        
-    Returns:
-    --------
-    tuple
-        (name, cleaned_content)
-    """
     import re
     name = None
     cleaned_content = content
